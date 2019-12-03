7be4ef2567cce7fc642af1382747e5d12c66dca6
Dmitry Sky <dmitry.sky@icloud.com>
2019-12-03 16:32:43 +1100

    render a list of items

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,8 +1,25 @@
 import React from 'react';
-import { Text, StyleSheet, View } from 'react-native';
+import { Text, StyleSheet, View, FlatList } from 'react-native';
 
 const ListScreen = () => {
-  return <Text>List</Text>;
+  const friends = [
+    {name: 'Friend #1'},
+    {name: 'Friend #2'},
+    {name: 'Friend #3'},
+    {name: 'Friend #4'},
+    {name: 'Friend #5'}
+  ];
+
+  return(
+    <FlatList
+      data={friends}
+      renderItem={( {item} ) => {
+        return (
+          <Text key={item.name}>{item.name}</Text>
+        );
+      }}
+    />
+  );
 };
 
 const styles = StyleSheet.create({});
