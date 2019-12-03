35592f2d580d0fac56e5da9b613a2efe4e88f4c4
Dmitry Sky <dmitry.sky@icloud.com>
2019-12-03 15:37:28 +1100

    can't display object in JSX but can display object param as long as it is not object, I know it's wicked

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { Text, StyleSheet, View } from 'react-native';
 
 const ComponentsScreen = () => {
+  const greeting = { color: 'Blue' }
+
   return (
       <View>
          <Text style={styles.textStyle}>This is the components Screen</Text>
+         <Text>{greeting.color}</Text>
       </View>
     );
 };
