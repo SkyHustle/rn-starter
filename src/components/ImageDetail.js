525778b2b01e21bcdfd384bb81cf1e46dd73a2e5
Dmitry Sky <dmitry.sky@icloud.com>
2019-12-04 15:24:20 +1100

    add image score prop to images

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Text, StyleSheet, View, Image } from 'react-native';
 
-const ImageDetail = (props) => {
+const ImageDetail = props => {
   return(
     <View>
       <Image source={props.imageSource} />
       <Text>{props.title}</Text>
+      <Text>{props.score}</Text>
     </View>
   );
 };
--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -5,18 +5,20 @@ import ImageDetail from '../components/ImageDetail'
 const ImageScreen = () => {
   return(
     <View>
-      <Text>Image Screen</Text>
       <ImageDetail
         title={'Beach'}
         imageSource={require('../../assets/beach.jpg')}
+        score={9}
       />
       <ImageDetail
         title={'Forest'}
         imageSource={require('../../assets/forest.jpg')}
+        score={7}
       />
       <ImageDetail
         title={'Mountain'}
         imageSource={require('../../assets/mountain.jpg')}
+        score={5}
       />
     </View>
   );
