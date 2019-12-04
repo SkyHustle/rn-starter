f579cbfee87018c2d2697d636026b543e5a5e861
Dmitry Sky <dmitry.sky@icloud.com>
2019-12-04 12:37:15 +1100

    create another button but this time using TouchableOpacity and console log something

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, StyleSheet, View, Button } from 'react-native';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
 const HomeScreen = () => {
   return (
@@ -9,6 +9,9 @@ const HomeScreen = () => {
         title="Go To Components Demo"
         onPress={() => console.log('Button Pressed')}
       />
+      <TouchableOpacity onPress={() => console.log('List Pressed')}>
+        <Text>Go To List Demo</Text>
+      </TouchableOpacity>
     </View>
   );
 };
