8fc0d40e5e49f2217931221e50533e9b40e1a0b4
Dmitry Sky <dmitry.sky@icloud.com>
2019-12-04 12:26:13 +1100

    add button to home screen that will go to componentsdemo

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View, Button } from 'react-native';
 
 const HomeScreen = () => {
-  return <Text style={styles.textStyle}>Boss Home Screen!!!</Text>;
+  return (
+    <View>
+      <Text style={styles.textStyle}>Boss Home Screen!!!</Text>
+      <Button title="Go To Components Demo"/>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
