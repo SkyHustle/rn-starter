2ebc658e4343a283e92edde24ef87f2469c549ba
Dmitry Sky <dmitry.sky@icloud.com>
2019-12-06 16:04:34 +1100

    clean up some shit

--- src/screens/CustomColorScreen.js
+++ src/screens/CustomColorScreen.js
@@ -18,12 +18,11 @@ const CustomColorScreen = () => {
           adjustColor(handleRed(color, 'less'));
         }}
       />
-      <Text>{`rgb(${color.red.toString()}, ${color.green.toString()}, ${color.blue.toString()})`}</Text>
+      <Text style={{textAlign: 'center', fontSize: 20}}>{`rgb(${color.red.toString()}, ${color.green.toString()}, ${color.blue.toString()})`}</Text>
       <View style={{
         height: 100,
         width: 100,
         borderWidth: 2,
-        textAlign: 'center',
         backgroundColor: `rgb(${color.red.toString()}, ${color.green.toString()}, ${color.blue.toString()})`
       }} />
     </View>
@@ -38,15 +37,6 @@ const handleRed = (color, adjustment) => {
   } else if (adjustment === 'less') {
     return updatedColor = {red: updatedColor.red - 20, green: 0, blue: 0}
   }
-
-  // return updatedColor;
-
-  // const red = Math.floor(Math.random() * 256)
-  // const green = Math.floor(Math.random() * 256)
-  // const blue = Math.floor(Math.random() * 256)
-
-  // return `rgb(${red}, ${green}, ${blue})`
-  // return 'rgb(255,200,200)'
 }
 
 
