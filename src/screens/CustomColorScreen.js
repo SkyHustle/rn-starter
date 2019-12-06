9d88ed68928db4f85cc81a97e34a99c0eb8f3dfb
Dmitry Sky <dmitry.sky@icloud.com>
2019-12-06 15:59:22 +1100

    make box more red and less red by clicking buttons

--- src/screens/CustomColorScreen.js
+++ src/screens/CustomColorScreen.js
@@ -1,35 +1,53 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Button } from 'react-native';
+import { StyleSheet, View, Button, Text } from 'react-native';
 
 const CustomColorScreen = () => {
-  const [color, adjustColor] = useState('rgb(50,0,0)');
+  const [color, adjustColor] = useState({red: 0, green: 0, blue: 0});
 
   return(
     <View>
       <Button
         title='More Red'
         onPress={() => {
-          adjustColor('rgb(150,0,0)');
+          adjustColor(handleRed(color, 'more'));
         }}
       />
       <Button
         title='Less Red'
         onPress={() => {
-          adjustColor('rgb(50,0,0)');
+          adjustColor(handleRed(color, 'less'));
         }}
       />
-      <View style={{ height: 100, width: 100, backgroundColor: color }} />
+      <Text>{`rgb(${color.red.toString()}, ${color.green.toString()}, ${color.blue.toString()})`}</Text>
+      <View style={{
+        height: 100,
+        width: 100,
+        borderWidth: 2,
+        textAlign: 'center',
+        backgroundColor: `rgb(${color.red.toString()}, ${color.green.toString()}, ${color.blue.toString()})`
+      }} />
     </View>
   );
 };
 
-// const randomRgb = () => {
-//   const red = Math.floor(Math.random() * 256)
-//   const green = Math.floor(Math.random() * 256)
-//   const blue = Math.floor(Math.random() * 256)
+const handleRed = (color, adjustment) => {
+  let updatedColor = color
 
-//   return `rgb(${red}, ${green}, ${blue})`
-// }
+  if (adjustment === 'more') {
+    return updatedColor = {red: updatedColor.red + 20, green: 0, blue: 0}
+  } else if (adjustment === 'less') {
+    return updatedColor = {red: updatedColor.red - 20, green: 0, blue: 0}
+  }
+
+  // return updatedColor;
+
+  // const red = Math.floor(Math.random() * 256)
+  // const green = Math.floor(Math.random() * 256)
+  // const blue = Math.floor(Math.random() * 256)
+
+  // return `rgb(${red}, ${green}, ${blue})`
+  // return 'rgb(255,200,200)'
+}
 
 
 export default CustomColorScreen;
