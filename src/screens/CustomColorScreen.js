f7b00fef6547e0bc5c93e32202eddee9911ff744
Dmitry Sky <dmitry.sky@icloud.com>
2019-12-07 14:37:29 +1100

    full blown boss refactor

--- src/screens/CustomColorScreen.js
+++ src/screens/CustomColorScreen.js
@@ -23,7 +23,13 @@ const CustomColorScreen = () => {
     switch (color) {
       case 'red':
         red + change > 255 || red + change < 0 ? null : setRed(red + change);
-      return;
+        return;
+      case 'green':
+        green + change > 255 || green + change < 0 ? null : setGreen(green + change);
+        return;
+      case 'blue':
+        blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change);
+        return;
     }
   };
 
@@ -36,13 +42,13 @@ const CustomColorScreen = () => {
       />
       <CustomColor
         color='Green'
-        onIncrease={() => setGreen(green + COLOR_INCREMENT)}
-        onDecrease={() => setGreen(green - COLOR_INCREMENT)}
+        onIncrease={() => setColor('green', COLOR_INCREMENT)}
+        onDecrease={() => setColor('green', -1 * COLOR_INCREMENT)}
       />
       <CustomColor
         color='Blue'
-        onIncrease={() => setBlue(blue + COLOR_INCREMENT)}
-        onDecrease={() => setBlue(blue - COLOR_INCREMENT)}
+        onIncrease={() => setColor('blue', COLOR_INCREMENT)}
+        onDecrease={() => setColor('blue', -1 * COLOR_INCREMENT)}
       />
 
       <Text style={{textAlign: 'center', fontSize: 20}}>{`rgb(${red}, ${green}, ${blue})`}</Text>
