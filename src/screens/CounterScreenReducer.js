0abddc7ba12cfd418c7534f8a13bb07810d31e9e
Dmitry Sky <dmitry.sky@icloud.com>
2019-12-08 12:27:40 +1100

    write Reducer and log the action to the terminal

--- src/screens/CounterScreenReducer.js
+++ src/screens/CounterScreenReducer.js
@@ -1,14 +1,28 @@
 import React, {useReducer} from 'react';
 import {StyleSheet, View, Text, Button} from 'react-native';
 
+const reducer = (state, action) => {
+  // state = {count:0}
+  //action = {type: increase_count || decrease_count, payload: number}
+  console.log(action)
+};
+
 const CounterScreenReducer = () => {
+  const [state, dispatch] = useReducer(reducer, {count:0})
+
   return(
     <View>
-      <Button title='Increase' />
-      <Button title='Decrease' />
-      <Text>Current Count:</Text>
+      <Button
+        title='Increase'
+        onPress={() => { dispatch({ type:'increase_count', payload: 1}) }}
+      />
+      <Button
+        title='Decrease'
+        onPress={() => {console.log('Decrease')}}
+      />
+      <Text style={{textAlign: 'center'}}>Current Count:</Text>
     </View>
   );
-}
+};
 
 export default CounterScreenReducer;
